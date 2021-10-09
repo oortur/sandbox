7bebc0c8722f6ff791896ea4dd47a8e80fc2e495
ZlataMoore <92014033+ZlataMoore@users.noreply.github.com>
2021-10-09 23:04:30 +0300

    Update zlata_moore.py

--- zlata_moore.py
+++ zlata_moore.py
@@ -1,8 +1,18 @@
-n = int(input())
-s = []
-k = 0
-while k != n:
-    b = input()
-    s.append(b)
-    k += 1
-print(s)
+class Complex:
+
+    def __init__(self, re=0, im=0):
+        self.re = re
+        self.im = im
+
+    def __str__(self):
+        return str(self.re) + ' ' + str(self.im) + 'i'
+
+    def __add__(self, other):
+        nr = self.re + other.re
+        ni = self.im + other.im
+        return Complex(nr, ni)
+
+    def __mul__(self, other):
+        nr = self.re * other.re - self.im * other.im
+        ni = self.re * other.im + self.im * other.re
+        return Complex(nr, ni)
